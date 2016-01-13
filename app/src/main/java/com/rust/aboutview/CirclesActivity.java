f979dce4ea8c87078e369fb9030846b5ae46bb53
RustFisher <rustfisher@foxmail.com>
2016-01-13 16:58:02 +0800

    Summary:add 3 circles
    [Type]:Feature
    
    [Description]:Add 3 circles
    modified:
    app/src/main/java/com/rust/aboutview/CirclesActivity.java
    app/src/main/java/com/rust/aboutview/view/OptionCircle.java
    app/src/main/res/layout/activity_circle_choose.xml

--- app/src/main/java/com/rust/aboutview/CirclesActivity.java
+++ app/src/main/java/com/rust/aboutview/CirclesActivity.java
@@ -1,26 +1,35 @@
 package com.rust.aboutview;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 
 import com.rust.aboutview.view.OptionCircle;
 
-
 public class CirclesActivity extends Activity {
 
-    private static final int CENTER_CIRCLE = 101;
+    private static final int UPDATE_ALL_CIRCLE = 99;
 
     OptionCircle centerCircle;
+    OptionCircle circle0;
+    OptionCircle circle1;
 
     CircleHandler handler = new CircleHandler(this);
 
     /**
-     * Handler : update UI
+     * Handler : update circles UI
      */
     static class CircleHandler extends Handler {
         CirclesActivity activity;
+        boolean zoomDir = true;
+        int r = 86;
+
+        int moveDir = 0;// 4 directions : 0 ~ 3
+        int circle1_x = 0;// offset value
+        int circle1_y = 0;
 
         CircleHandler(CirclesActivity a) {
             activity = a;
@@ -29,10 +38,43 @@ public class CirclesActivity extends Activity {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                case CENTER_CIRCLE: {
-
+                case UPDATE_ALL_CIRCLE: {
+                    if (zoomDir) {
+                        r++;
+                        if (r >= 99) zoomDir = false;
+                    } else {
+                        r--;
+                        if (r <= 86) zoomDir = true;
+                    }
+                    activity.circle0.invalidate();
+                    activity.circle0.setRadius(r);
+                    calOffsetX();
+                    activity.circle1.invalidate();
+                    activity.circle1.setCenterOffset(circle1_x, circle1_y);
                 }
+            }
+        }
 
+        private void calOffsetX() {
+            if (moveDir == 0) {
+                circle1_x--;
+                circle1_y++;
+                if (circle1_x <= -6) moveDir = 1;
+            }
+            if (moveDir == 1) {
+                circle1_x++;
+                circle1_y++;
+                if (circle1_x >= 0) moveDir = 2;
+            }
+            if (moveDir == 2) {
+                circle1_x++;
+                circle1_y--;
+                if (circle1_x >= 6) moveDir = 3;
+            }
+            if (moveDir == 3) {
+                circle1_x--;
+                circle1_y--;
+                if (circle1_x <= 0) moveDir = 0;
             }
         }
     }
@@ -43,10 +85,10 @@ public class CirclesActivity extends Activity {
         public void run() {
             while (true) {
                 Message message = new Message();
-                message.what = CENTER_CIRCLE;
+                message.what = UPDATE_ALL_CIRCLE;
                 handler.sendEmptyMessage(message.what);
                 try {
-                    Thread.sleep(10); // pause
+                    Thread.sleep(160); // pause
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
@@ -59,10 +101,30 @@ public class CirclesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle_choose);
         centerCircle = (OptionCircle) findViewById(R.id.center_circle);
-        centerCircle.invalidate();
-        centerCircle.setRadius(58);
+        circle0 = (OptionCircle) findViewById(R.id.circle_0);
+        circle1 = (OptionCircle) findViewById(R.id.circle_1);
 
+        centerCircle.setRadius(69);
+        centerCircle.setColorText(Color.BLUE);
+        centerCircle.setColorCircle(Color.BLUE);
+        centerCircle.setText("点击圈圈");
+
+        circle0.setColorText(Color.RED);
+        circle0.setText("RED");
+
+        circle1.setColorCircle(Color.GREEN);
+        circle1.setColorText(Color.GREEN);
+        circle1.setText("Green");
+        circle1.setRadius(95);
+
+        circle0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Thread t = new Thread(new UpdateCircles());
+        t.start();
     }
-
-
 }
--- app/src/main/java/com/rust/aboutview/view/OptionCircle.java
+++ app/src/main/java/com/rust/aboutview/view/OptionCircle.java
@@ -2,7 +2,9 @@ package com.rust.aboutview.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -11,6 +13,12 @@ public class OptionCircle extends View {
     private final Paint paint;
     private final Context context;
     int radius = -1;
+    int centerOffsetX = 0;
+    int centerOffsetY = 0;
+    int colorCircle;
+    int colorBackground = Color.WHITE;
+    int colorText;
+    String textCircle = "";
 
     public OptionCircle(Context context) {
         this(context, null);
@@ -22,31 +30,64 @@ public class OptionCircle extends View {
         this.paint = new Paint();
         this.paint.setAntiAlias(true);
         this.paint.setStyle(Paint.Style.STROKE);
+        colorCircle = Color.argb(205, 245, 2, 51);
+        colorText = colorCircle;
     }
 
     public void setRadius(int r) {
         this.radius = r;
     }
 
+    public void setCenterOffset(int x, int y) {
+        this.centerOffsetX = x;
+        this.centerOffsetY = y;
+    }
+
+    public void setColorCircle(int c) {
+        this.colorCircle = c;
+    }
+
+    public void setColorText(int c) {
+        this.colorText = c;
+    }
+
+    public void setColorBackground(int c) {
+        this.colorBackground = c;
+    }
+
+    public void setText(String s) {
+        this.textCircle = s;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         int center = getWidth() / 2;
-        int innerCircle = 69;
+        int innerCircle = 86;
         if (radius > 0) {
             innerCircle = dip2px(context, radius); //set inner radius
         }
 
         // draw circle
-        this.paint.setARGB(255, 167, 190, 206);
-        this.paint.setStrokeWidth(2);
-        canvas.drawCircle(center, center, innerCircle, this.paint);
+        this.paint.setStyle(Paint.Style.STROKE);
+        this.paint.setColor(colorCircle);
+        this.paint.setStrokeWidth(1.5f);
+        canvas.drawCircle(center + centerOffsetX, center + centerOffsetY, innerCircle, this.paint);
 
+        // draw text
+        this.paint.setStyle(Paint.Style.FILL);
+        this.paint.setStrokeWidth(1);
+        this.paint.setTextSize(22);
+        this.paint.setTypeface(Typeface.MONOSPACE);
+        this.paint.setColor(colorText);
+        this.paint.setTextAlign(Paint.Align.CENTER);
+        Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
+        canvas.drawText(textCircle, center + centerOffsetX,
+                center + centerOffsetY - (fontMetrics.top + fontMetrics.bottom) / 2, this.paint);
         super.onDraw(canvas);
     }
 
-
     /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     * convert dp to px
      */
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
