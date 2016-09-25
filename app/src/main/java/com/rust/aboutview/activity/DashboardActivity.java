71625b7170c844aec70050df29e5d511e4f2e129
RustFisher <rustfisher@foxmail.com>
2016-09-25 16:44:42 +0800

    fix dimension bug

--- app/src/main/java/com/rust/aboutview/activity/DashboardActivity.java
+++ app/src/main/java/com/rust/aboutview/activity/DashboardActivity.java
@@ -11,6 +11,7 @@ import com.rustfisher.fisherandroidchart.DashboardProgressView;
 public class DashboardActivity extends Activity {
 
     private DashboardProgressView mDash1;
+    private DashboardProgressView mDash2;
     private DashHandler mDashHandler;
 
     @Override
@@ -18,21 +19,24 @@ public class DashboardActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_dashboard);
         mDashHandler = new DashHandler(this);
+
         mDash1 = (DashboardProgressView) findViewById(R.id.dashboard_1);
+        mDash2 = (DashboardProgressView) findViewById(R.id.dashboard_2);
 
         mDash1.setHeaderTitle("dash1");
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         dashThread1.start();
+        dashThread2.start();
     }
 
     @Override
     protected void onDestroy() {
         dashThread1.interrupt();
+        dashThread2.interrupt();
         super.onDestroy();
     }
 
@@ -42,7 +46,7 @@ public class DashboardActivity extends Activity {
             for (int i = 0; i < 104; i++)
                 try {
                     Thread.sleep(500L);
-                    mDashHandler.obtainMessage(DashHandler.REFRESH_DASH, i, -1).sendToTarget();
+                    mDashHandler.obtainMessage(DashHandler.REFRESH_DASH_1, i, -1).sendToTarget();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     break;
@@ -50,8 +54,24 @@ public class DashboardActivity extends Activity {
         }
     };
 
+    private Thread dashThread2 = new Thread() {
+        @Override
+        public void run() {
+            for (int i = 104; i > -20; i--)
+                try {
+                    Thread.sleep(50L);
+                    mDashHandler.obtainMessage(DashHandler.REFRESH_DASH_2, i, -1).sendToTarget();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+        }
+    };
+
+
     static class DashHandler extends Handler {
-        public static final int REFRESH_DASH = 111;
+        public static final int REFRESH_DASH_1 = 100;
+        public static final int REFRESH_DASH_2 = 200;
 
         static DashboardActivity act;
 
@@ -63,8 +83,11 @@ public class DashboardActivity extends Activity {
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             switch (msg.what) {
-                case REFRESH_DASH:
+                case REFRESH_DASH_1:
                     act.mDash1.setRealTimeValue(msg.arg1);
+                    break;
+                case REFRESH_DASH_2:
+                    act.mDash2.setRealTimeValue(msg.arg1);
                     break;
             }
         }
