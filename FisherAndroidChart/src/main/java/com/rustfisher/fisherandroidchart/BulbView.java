e7626a04d54597e366ea087a92c8c83695ae0944
RustFisher <rustfisher@foxmail.com>
2016-10-08 10:43:28 +0800

    update bulb style

--- FisherAndroidChart/src/main/java/com/rustfisher/fisherandroidchart/BulbView.java
+++ FisherAndroidChart/src/main/java/com/rustfisher/fisherandroidchart/BulbView.java
@@ -7,12 +7,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.widget.ImageView;
 
 public class BulbView extends ImageView {
 
-    //    private static final String TAG = BulbView.class.getSimpleName();
+    private static final String TAG = BulbView.class.getSimpleName();
     private float lineWid;
     private float botArcR;
     private float botLineLen;
@@ -45,7 +46,7 @@ public class BulbView extends ImageView {
         lineWid = a.getDimensionPixelSize(R.styleable.BulbView_lineWid, (int) dpToPx(2));
         botArcR = a.getDimensionPixelSize(R.styleable.BulbView_botArcRadius, (int) dpToPx(7));
         botLineLen = a.getDimensionPixelSize(R.styleable.BulbView_botLineLen, (int) dpToPx(14));
-        midHeight = a.getDimensionPixelSize(R.styleable.BulbView_midHeight, (int) dpToPx(28));
+        midHeight = a.getDimensionPixelSize(R.styleable.BulbView_midHeight, (int) dpToPx(24));
         shineLen = a.getDimensionPixelSize(R.styleable.BulbView_shineLen, (int) (botLineLen * 0.85));
         wholeSizeRatio = a.getFloat(R.styleable.BulbView_wholeSizeRatio, 1.0f);
         bulbOutlineColor = a.getColor(R.styleable.BulbView_bulbOutlineColor, Color.WHITE);
@@ -98,6 +99,12 @@ public class BulbView extends ImageView {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.d(TAG, getId() + " onSizeChanged: [" + w + ", " + h + "]");
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         int viewWid = getWidth();
@@ -106,35 +113,35 @@ public class BulbView extends ImageView {
         /**
          * Draw background
          */
-        float originX = viewWid / 2;
-        float originY = viewHeight;
-        float botP1_x = (float) (originX - botLineLen / 2.0);// Left
-        float botP1_y = originY;
-        float botP2_x = (float) (originX + botLineLen / 2.0);// Right
-        float botP2_y = originY;
-        float midP1_x = (float) (originX + botLineLen / 2.0 + botArcR);
-        float midP1_y = botP2_y - botArcR - midHeight;
-        float midP2_x = botP2_x + botArcR;
-        float midP2_y = botP2_y - botArcR;
-        float midP3_x = (float) (originX - botLineLen / 2.0 - botArcR);
-        float midP3_y = originY - botArcR;
-        float midP4_x = (float) (originX - botLineLen / 2.0 - botArcR);
-        float midP4_y = originY - botArcR - midHeight;
+        float bulbOriginX = viewWid / 2;
+        float bulbOriginY = viewHeight;
+        float bulbBotP1_x = (float) (bulbOriginX - botLineLen / 2.0);// Left
+        float bulbBotP1_y = bulbOriginY;
+        float bulbBotP2_x = (float) (bulbOriginX + botLineLen / 2.0);// Right
+        float bulbBotP2_y = bulbOriginY;
+        float bulbMidP1_x = (float) (bulbOriginX + botLineLen / 2.0 + botArcR);
+        float bulbMidP1_y = bulbBotP2_y - botArcR - midHeight;
+        float bulbMidP2_x = bulbBotP2_x + botArcR;
+        float bulbMidP2_y = bulbBotP2_y - botArcR;
+        float bulbMidP3_x = (float) (bulbOriginX - botLineLen / 2.0 - botArcR);
+        float bulbMidP3_y = bulbOriginY - botArcR;
+        float bulbMidP4_x = (float) (bulbOriginX - botLineLen / 2.0 - botArcR);
+        float bulbMidP4_y = bulbOriginY - botArcR - midHeight;
 
-        canvas.drawLine(botP1_x, botP1_y, botP2_x + 2, botP2_y, bgPaint); // Add offset
+        canvas.drawLine(bulbBotP1_x, bulbBotP1_y, bulbBotP2_x + 2, bulbBotP2_y, bgPaint); // Add offset
 
-        botLeftRect.set(midP3_x, viewHeight - 2 * botArcR, botP1_x + botArcR, botP1_y);
+        botLeftRect.set(bulbMidP3_x, viewHeight - 2 * botArcR, bulbBotP1_x + botArcR, bulbBotP1_y);
         canvas.drawArc(botLeftRect, 89, 91, false, bgPaint);
 
-        botRightRect.set(botP2_x - botArcR, viewHeight - 2 * botArcR, midP1_x, botP2_y);
+        botRightRect.set(bulbBotP2_x - botArcR, viewHeight - 2 * botArcR, bulbMidP1_x, bulbBotP2_y);
         canvas.drawArc(botRightRect, -1, 91, false, bgPaint);
 
-        canvas.drawLine(midP3_x, midP3_y, midP4_x, midP4_y, bgPaint);
-        canvas.drawLine(midP2_x, midP2_y, midP1_x, midP1_y, bgPaint);
-        canvas.drawLine(midP4_x - lineWid / 2, midP4_y, midP1_x + lineWid / 2, midP1_y, bgPaint);
+        canvas.drawLine(bulbMidP3_x, bulbMidP3_y, bulbMidP4_x, bulbMidP4_y, bgPaint);
+        canvas.drawLine(bulbMidP2_x, bulbMidP2_y, bulbMidP1_x, bulbMidP1_y, bgPaint);
+        canvas.drawLine(bulbMidP4_x - lineWid / 2, bulbMidP4_y, bulbMidP1_x + lineWid / 2, bulbMidP1_y, bgPaint);
 
         // Draw bulb head background
-        headRect.set(midP4_x, (float) (midP4_y - (midP1_x - midP4_x) / 2.0), midP1_x, (float) (midP4_y + (midP1_x - midP4_x) / 2.0));
+        headRect.set(bulbMidP4_x, (float) (bulbMidP4_y - (bulbMidP1_x - bulbMidP4_x) / 2.0), bulbMidP1_x, (float) (bulbMidP4_y + (bulbMidP1_x - bulbMidP4_x) / 2.0));
         canvas.drawArc(headRect, 180, 180, false, bgPaint);
 
         /**
@@ -144,19 +151,19 @@ public class BulbView extends ImageView {
         shinePaint.setStyle(Paint.Style.STROKE);
         float shineLineWid = (float) (1.4 * lineWid);
         shinePaint.setStrokeWidth(shineLineWid);
-        headShineRectF.set((midP4_x + lineWid),
-                (float) (midP4_y - (midP1_x - midP4_x) / 2.0 + lineWid),
-                (midP1_x - lineWid),
-                (float) (midP4_y + (midP1_x - midP4_x) / 2.0 - lineWid * 2));
+        headShineRectF.set((bulbMidP4_x + lineWid),
+                (float) (bulbMidP4_y - (bulbMidP1_x - bulbMidP4_x) / 2.0 + lineWid),
+                (bulbMidP1_x - lineWid),
+                (float) (bulbMidP4_y + (bulbMidP1_x - bulbMidP4_x) / 2.0 - lineWid * 2));
         canvas.drawArc(headShineRectF, 180, 180, false, shinePaint);
         shinePaint.setStyle(Paint.Style.FILL_AND_STROKE);// Draw inner color
-        headShineRectF.set((midP4_x + lineWid),
-                (float) (midP4_y - (midP1_x - midP4_x) / 2.0 + lineWid),
-                (midP1_x - lineWid),
-                (float) (midP4_y + (midP1_x - midP4_x) / 2.0 - lineWid * 2 + 2)); // Bottom add offset
+        headShineRectF.set((bulbMidP4_x + lineWid),
+                (float) (bulbMidP4_y - (bulbMidP1_x - bulbMidP4_x) / 2.0 + lineWid),
+                (bulbMidP1_x - lineWid),
+                (float) (bulbMidP4_y + (bulbMidP1_x - bulbMidP4_x) / 2.0 - lineWid * 2 + 2)); // Bottom add offset
         canvas.drawArc(headShineRectF, 180, 180, false, shinePaint);
 
-        drawShiningLines(canvas, midP1_x, midP1_y, midP4_x, midP4_y);
+        drawShiningLines(canvas, bulbMidP1_x, bulbMidP1_y, bulbMidP4_x, bulbMidP4_y);
     }
 
     /**
