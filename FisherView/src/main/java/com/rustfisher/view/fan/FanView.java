a85172971c8df9d5d45c0bfbbbeb942255bbdf3b
RustFisher <rustfisher@foxmail.com>
2019-06-15 10:27:56 +0800

    FisherView update FanView

--- FisherView/src/main/java/com/rustfisher/view/fan/FanView.java
+++ FisherView/src/main/java/com/rustfisher/view/fan/FanView.java
@@ -9,7 +9,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.rustfisher.view.FSUtils;
@@ -20,15 +19,14 @@ import com.rustfisher.view.R;
  * Created by Rust Fisher on 2017/7/20.
  */
 public class FanView extends View {
-
-    private static final String TAG = "rustApp";
+//    private static final String TAG = "rustApp";
     private Paint mPaint = new Paint(); // main paint
-    private float paintStrokeWid = 2.4f;
-    private float propBotWid = 10f; // Pixel - Wid of prop bottom
-    private float propTopWid = propBotWid * 0.85f;  // Pixel - Wid of prop top
-    private float dotHeightRatio = 0.6f;    // Ratio, dot in the view
+    float paintStrokeWid = 2.4f;
+    private float propBotWid;     // Pixel - Wid of prop bottom
+    private float propTopWid;     // Pixel - Wid of prop top
+    float dotHeightRatio = 0.6f;  // Ratio, dot in the view
     private int fanDegree = 60;
-    private int propColor = Color.BLACK;
+    private int propColor;
 
     Path propPath = new Path();
     Path fanPath = new Path();
@@ -68,12 +66,10 @@ public class FanView extends View {
 
         mPaint.setStrokeWidth(paintStrokeWid);
         mPaint.setColor(propColor);
-        float cx = getWidth() / 2.0f;
-        float cy = getHeight() / 2.0f;
+        float cx = getWidth() / 2.0f; // “发动机”位置的x坐标
         float propHeight = getHeight() * dotHeightRatio; // The height of prop
         float fanLen = propHeight / 2; // The length of fan
 
-        float dotX = cx; // The dot - generator location
         float dotY = getHeight() - propHeight;
 
         // Draw the prop
@@ -88,12 +84,55 @@ public class FanView extends View {
         canvas.drawPath(propPath, mPaint);
 
         // Draw the dot
-        canvas.drawCircle(dotX, dotY, propTopWid * 0.55f, mPaint);
+        canvas.drawCircle(cx, dotY, propTopWid * 0.55f, mPaint);
 
         // Draw the fans
         for (int i = 0; i < 3; i++) {
-            drawFan(canvas, dotX, dotY, fanLen, fanDegree + i * 120);
+            if (i == 0) {
+                canvas.rotate(fanDegree + 120, cx, dotY);
+            } else {
+                canvas.rotate(120, cx, dotY);
+            }
+            drawOriginPaddle(canvas, cx, dotY, fanLen);
         }
+        // 画桨叶的时候已经把画布旋转了，如果后续还要绘制其他元素，需要把画布旋转回来
+        canvas.rotate(-(fanDegree + 3 * 120), cx, dotY);
+//        canvas.drawCircle(dotX + fanLen, dotY, 4, mPaint);
+    }
+
+    /**
+     * 绘制0角度的一个桨叶
+     *
+     * @param canvas 画布；一般是已经旋转到了目的角度
+     * @param dotX   圆心x
+     * @param dotY   圆心y
+     * @param fanLen 桨叶的长度
+     */
+    private void drawOriginPaddle(Canvas canvas, float dotX, float dotY, float fanLen) {
+        double turnLen = fanLen * 0.25;
+        int degreeSpan = (int) Math.toDegrees(Math.atan(propBotWid / 2 / turnLen)); // 中间的节点
+        int startDegreeSpan = (degreeSpan * 2);
+
+        float startLeftX = (float) (dotX + propTopWid / 2 * Math.sin(Math.toRadians(startDegreeSpan)));
+        float startLeftY = (float) (dotY + propTopWid / 2 * Math.cos(Math.toRadians(startDegreeSpan)));
+        float startRightX = (float) (dotX + propTopWid / 2 * Math.sin(Math.toRadians(-startDegreeSpan)));
+        float startRightY = (float) (dotY + propTopWid / 2 * Math.cos(Math.toRadians(-startDegreeSpan)));
+
+        float endY = (dotY + fanLen);
+        float leftSpanX = (float) (dotX + turnLen * Math.sin(Math.toRadians(degreeSpan)));
+        float leftSpanY = (float) (dotY + turnLen * Math.cos(Math.toRadians(degreeSpan)));
+        float rightSpanX = (float) (dotX + turnLen * Math.sin(Math.toRadians(-degreeSpan)));
+        float rightSpanY = (float) (dotY + turnLen * Math.cos(Math.toRadians(-degreeSpan)));
+
+        fanPath.reset();
+        fanPath.moveTo(startLeftX, startLeftY);
+        fanPath.lineTo(leftSpanX, leftSpanY);
+        fanPath.lineTo(dotX, endY);
+        fanPath.lineTo(rightSpanX, rightSpanY);
+        fanPath.lineTo(startRightX, startRightY);
+        fanPath.lineTo(dotX, dotY);
+        fanPath.close();
+        canvas.drawPath(fanPath, mPaint);
     }
 
     /**
@@ -102,7 +141,9 @@ public class FanView extends View {
      * @param dotY   The generator
      * @param fanLen Length of fan
      * @param degree Angle 0 ~ 360
+     * @deprecated see drawOriginPaddle
      */
+    @SuppressWarnings("unused")
     private void drawFan(Canvas canvas, float dotX, float dotY, float fanLen, float degree) {
         double radians = Math.toRadians(degree);
         double turnLen = fanLen * 0.25;
